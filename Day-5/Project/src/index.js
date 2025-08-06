543b75a9cb72789d661184e4297c8c7765bc594e
mrsiddharthsolanki <solankisiddharth059@gmail.com>
2025-08-06 15:19:24 +0530

    fix: Correct yargs command initialization and improve directory listing functionality

--- Day-5/Project/src/index.js
+++ Day-5/Project/src/index.js
@@ -2,42 +2,44 @@ import fs from "fs"
 // import { type } from "os";
 import path from "path"
 import yargs from "yargs"
+import { hideBin } from "yargs/helpers"
 
 // console.log(yargs.());
 
 
-yargs.command( 'ls [directory]', 'List contents of a directory' ,(yargs) => {
+yargs(hideBin(process.argv))
+    .command( 'ls [directory]', 'List contents of a directory' ,(yargs) => {
     yargs.positional("directory", {
         discribe : "A directory To List",
         default :"."
     })
-}, (argv) => {
-    const tergetDir = path.resolve(argv.directory);
+    }, (argv) => {
+        const tergetDir = path.resolve(argv.directory);
 
-    fs.readdir(tergetDir , (err, files) => {
-        if(err){
-            console.log("Error Listen The Directory :", err.message);
-            return;
-        } else {
-            files.forEach( (file) => console.log(file))
-        }
-    })
-}).command('mkdir <name>', 'Create a new directory' , (yargs) => {
-    yargs.positional( "name" , {
-        discribe : "A name of The new Directory",
-        type : string
-    })
-}, (argv) => {
-    fs.mkdir(argv.name, (err) => {
-        if(err) {
-            console.log("Error Creating The Directory:", err.message);
-            return;
-        }
+        fs.readdir(tergetDir , (err, files) => {
+            if(err){
+                console.log("Error Listen The Directory :", err.message);
+                return;
+            } else {
+                files.forEach( (file) => console.log(file))
+            }
+        })
+    }).command('mkdir <name>', 'Create a new directory' , (yargs) => {
+        yargs.positional( "name" , {
+            discribe : "A name of The new Directory",
+            type : String
+        })
+    }, (argv) => {
+        fs.mkdir(argv.name, (err) => {
+            if(err) {
+                console.log("Error Creating The Directory:", err.message);
+                return;
+            }
 
-        console.log(`Directory ${argv.name} Created`);
-        
+            console.log(`Directory ${argv.name} Created`);
+            
+        })
     })
-})
-.help
-.argv
+    .help()
+    .argv
 
